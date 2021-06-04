84d851f9d88e99e884dc6cc38a5638af0c29da9c
The Big Brain <>
2021-06-04 19:09:27 +0000

    Removed setxp command for now.

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ async def joined(ctx, member: guilded.Member):
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
 #Guilded Only
+"""
 @bot.command()
 async def setxp(ctx, member: guilded.Member, xp: int):
   try:
@@ -37,6 +38,6 @@ async def setxp(ctx, member: guilded.Member, xp: int):
     await ctx.send("There was an error. I may have insufficient role permissions. Please fix the issue and try again.")
   else:
     await ctx.send(f"Successfully set {member.name}'s XP to {xp}.")
-
+"""
 
 bot.run('xenigib207@quossum.com', 'KOOLIOMAN123')
